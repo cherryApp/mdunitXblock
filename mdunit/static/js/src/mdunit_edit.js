4659706f8b590a5b76d41143ac4386791d06e3fb
Cserkó József <j.cserko@gmail.com>
2019-02-18 19:39:01 +0100

    Feat: handle files.

--- mdunit/static/js/src/mdunit_edit.js
+++ mdunit/static/js/src/mdunit_edit.js
@@ -1,15 +1,27 @@
 // Init settings.
 function mdUnitEdit(runtime, element) {
-    
-    
+
+
     $(function ($) {
         console.log("The xblock editor has been initialized.");
         console.log("Element, runtime: ", element, runtime);
-        element.find("#file_upload_input").on("change", handleFileUpload);
+
+        var input = element.find("#file_upload_input");
+        input.on("change", handleFileUpload);
+        element.find(".file-trigger").on("click", function () {
+            input.click();
+        });
     });
 }
 
 // Handle file upload.
 function handleFileUpload(ev) {
-    console.log(ev, this);
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        // The file's text will be printed here
+        console.log(e.target.result);
+    };
+    
+    reader.readAsText(file);
 }
