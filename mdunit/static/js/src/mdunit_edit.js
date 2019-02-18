42bbb3d3216d46dccfbbebcd48b5d00c4af791ef
Cserkó József <j.cserko@gmail.com>
2019-02-18 19:23:49 +0100

    f

--- mdunit/static/js/src/mdunit_edit.js
+++ mdunit/static/js/src/mdunit_edit.js
@@ -5,5 +5,11 @@ function mdUnitEdit(runtime, element) {
     $(function ($) {
         console.log("The xblock editor has been initialized.");
         console.log("Element, runtime: ", element, runtime);
+        element.find("#file_upload_input").on("change", handleFileUpload);
     });
+}
+
+// Handle file upload.
+function handleFileUpload(ev) {
+    console.log(ev, this);
 }
