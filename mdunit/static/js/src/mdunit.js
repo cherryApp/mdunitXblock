5268a39cba6444fb5607a3ea1fff94226114ecc9
Cserkó József <j.cserko@gmail.com>
2019-02-18 12:15:25 +0100

    Feat: init.

---
+++ mdunit/static/js/src/mdunit.js
@@ -0,0 +1,22 @@
+/* Javascript for {{cookiecutter.class_name}}. */
+function mdUnit(runtime, element) {
+
+    function updateCount(result) {
+        $('.count', element).text(result.count);
+    }
+
+    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+
+    $('p', element).click(function(eventObject) {
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({"hello": "world"}),
+            success: updateCount
+        });
+    });
+
+    $(function ($) {
+        console.log("The xblock has been initialized.");
+    });
+}
