8002b96cfdcd7392754d20745df912004c555602
Cserkó József <j.cserko@gmail.com>
2019-02-18 15:31:24 +0100

    fix

--- mdunit/mdunit.py
+++ mdunit/mdunit.py
@@ -69,7 +69,7 @@ class mdUnit(XBlock):
 
     def studio_view(self, context=None):
         self.md_url = 'https://xlearning.training360.com/asset-v1:Training360+1001+2019_T1+type@asset+block@14_html_form.md'
-        contents = urllib2.urlopen(self.md_url).read()
+        # contents = urllib2.urlopen(self.md_url).read()
 
         '''
         md = open(self.md_url, "r")
@@ -79,7 +79,7 @@ class mdUnit(XBlock):
         context = {
             'display_name': self.display_name,
             'md_url': self.md_url,
-            'md_content': contents
+            'md_content': __name__
         }
 
         html = self.resource_string("static/html/mdunit_edit.html")
